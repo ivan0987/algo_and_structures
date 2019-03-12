e4fee7785d72df3e5b6489e3727f0478783fd477
ivan <ivn0987@mail.ru>
2019-03-12 20:49:15 +0300

    Less_1

--- Lesson_1/1.py
+++ Lesson_1/1.py
@@ -1,2 +1,19 @@
 # 1.	Найти сумму и произведение цифр трехзначного числа, которое вводит пользователь.
 
+#Схема на скриншоте 1.png
+
+a = int(input('Введите трехзначное число: '))
+my_sum = a % 10
+my_prod = a % 10
+
+a = a // 10
+
+my_sum = my_sum + a % 10
+my_prod = my_prod * a % 10
+
+a = a // 10
+
+my_sum = my_sum + a % 10
+my_prod = my_prod * a % 10
+
+print(my_sum, my_prod)
